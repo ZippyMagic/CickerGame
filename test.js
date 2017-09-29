8991666ce5a19403b5e96e41334edb2e162c6d6e
ZippyMagic <zippymagician1@gmail.com>
2017-09-29 10:43:12 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-var
+var rewards = [
+	"Test1",
+	"Test2",
+	"Test3",
+	"Test4",
+	"Test5",
+	"Test6",
+	"Test7",
+	"Test8",
+	"Test9"
+]
+var rewardRare = [
+	//Coming soon
+]
+
+function pickRandReward() {
+	var i = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
+	alert(rewards[i]);
+}
+
+pickRandReward();
