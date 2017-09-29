703b571705dc7ce0c59020d8266dfa8a685921bb
ZippyMagic <zippymagician1@gmail.com>
2017-09-29 13:30:07 -0400

    Update dev.js

--- devBuild/dev.js
+++ devBuild/dev.js
@@ -1,24 +1,60 @@
-var rewards = {
-	knife1: {name: "Test1"},
-	knife2: {name: "Test2"},
-	knife3: {name: "Test3"},
-	knife4: {name: "Test4"},
-	knife5: {name: "Test5"}
+var item = {
+	knife1: {name: "Test1", rarity: "Common"},
+	knife2: {name: "Test2", rarity: "Rare"},
+	knife3: {name: "Test3", rarity: "Epic"},
+	knife4: {name: "Test4", rarity: "Legendary"},
+	knife5: {name: "Test5", rarity: "Unique"}
 };
-var rewardRare = [
-	//Coming soon
-];
+
 var money = 5;
 var multiplier = 1;
+var i;
 
+function applyRarity() {
+  if(item["knife" + i].rarity !== "Common") {
+	    var pick = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+	    
+	    if(item["knife" + i].rarity === "Rare") {
+	      if(pick <= 45) {
+	        return;
+	      } else {
+	        i = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+	        applyRarity();
+	      }
+	    } else if(item["knife" + i].rarity === "Epic") {
+	      if(pick <= 20) {
+	        return;
+	      } else {
+	        i = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+	        applyRarity()
+	      }
+	    } else if(item["knife" + i].rarity === "Legendary") {
+	      if(pick <= 7) {
+	        return;
+	      } else {
+	        i = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+	        applyRarity()
+	      }
+	    } else if(item["knife" + i].rarity === "Unique") {
+	      if(pick === 1) {
+	        return;
+	      } else {
+	        i = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+	        applyRarity()
+	      }
+	    }
+	  }
+}
 function pickRandReward() {
 	if(money > 4) {
 	  var money2 = money;
 	  money = money2 - 5;
-	  var i = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-	  console.log(money);
-	  console.log(i)
-	  console.log(rewards["knife" + i].name); // Can increase '5' and '1' to increase value
+	  i = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+	  
+	  applyRarity()
+	  console.log("money: " + money);
+	  console.log("i: " + i);
+	  console.log("knife: " + item["knife" + i].name); // Can increase '5' and '1' to increase value
 	}
 }
 function clickMoney(mult) {
